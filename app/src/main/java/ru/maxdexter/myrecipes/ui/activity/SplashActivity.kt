29f6a9ceb88f1b7f242db4a13c270725a21532bd
maxdexter <Maximusnew84!>
2021-06-16 10:35:51 +0300

    change themes.xml add new font

--- app/src/main/java/ru/maxdexter/myrecipes/ui/activity/SplashActivity.kt
+++ app/src/main/java/ru/maxdexter/myrecipes/ui/activity/SplashActivity.kt
@@ -1,12 +1,32 @@
 package ru.maxdexter.myrecipes.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import ru.maxdexter.myrecipes.R
+import ru.maxdexter.myrecipes.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+
+            )
+        }
+
     }
 }
