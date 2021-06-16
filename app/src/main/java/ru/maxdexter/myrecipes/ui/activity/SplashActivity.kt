f73277b504ec12b541b09d67aaebe7845c910080
maxdexter <Maximusnew84!>
2021-06-16 09:40:50 +0300

    added SplashActivity.kt

---
+++ app/src/main/java/ru/maxdexter/myrecipes/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package ru.maxdexter.myrecipes.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.maxdexter.myrecipes.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
