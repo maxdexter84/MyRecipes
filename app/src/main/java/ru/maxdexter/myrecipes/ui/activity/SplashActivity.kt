3431ace16810ce9726293a88ffb7fb6a9fb07ae4
maxdexter <Maximusnew84!>
2021-06-16 11:45:51 +0300

    added animation

--- app/src/main/java/ru/maxdexter/myrecipes/ui/activity/SplashActivity.kt
+++ app/src/main/java/ru/maxdexter/myrecipes/ui/activity/SplashActivity.kt
@@ -1,10 +1,17 @@
 package ru.maxdexter.myrecipes.ui.activity
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
+import ru.maxdexter.myrecipes.MainActivity
 import ru.maxdexter.myrecipes.R
 import ru.maxdexter.myrecipes.databinding.ActivitySplashBinding
 
@@ -17,9 +24,32 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+        hideStatusBar()
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        binding.tvAppName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+
+
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }else{
+        } else {
             @Suppress("DEPRECATION")
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -27,6 +57,5 @@ class SplashActivity : AppCompatActivity() {
 
             )
         }
-
     }
 }
